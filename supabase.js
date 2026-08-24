e375ac3a3e11e7f1ca2e74fb31fd9d8df58c6dcd
Rudar <rudar6511@gmail.com>
2026-08-24 16:58:21 +0530

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,7 +1,12 @@
-import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
+import { createClient } from
+"https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
 
-const supabaseUrl = "https://yefjfcrevavskvxzkkub.supabase.co";
+const SUPABASE_URL =
+"https://yefjfcrevavskvxzkkub.supabase.co";
 
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InllZmpmY3JldmF2c2t2eHpra3ViIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODM3NzUxMjcsImV4cCI6MjA5OTM1MTEyN30.w7n3wBB4rEbq8Cg2RpOlg-bNnLjwrGIuWajAfhLhiUE";
+const SUPABASE_ANON_KEY =
+eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InllZmpmY3JldmF2c2t2eHpra3ViIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODM3NzUxMjcsImV4cCI6MjA5OTM1MTEyN30.w7n3wBB4rEbq8Cg2RpOlg-bNnLjwrGIuWajAfhLhiUE
 
-export const supabase = createClient(supabaseUrl, supabaseKey);
+export const supabase = createClient(
+    SUPABASE_URL,
+    SUPABASE_ANON_KEY
